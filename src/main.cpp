c1ab3b093343734f945394189af4d99942294349
Noah <ntallsbrook@tuta.com>
2026-03-13 17:57:56 -0400

    im so done with this

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <LiquidCrystal.h>
 
 #define LED 3
+#define FAIL_LED 4
 #define RPM_INT 18 
 #define IR_VCC 11
 #define IR_FREQ 600
@@ -9,10 +10,19 @@
 
 #define LCD_EN 7
 #define LCD_RST 6
+#define D4 36
+#define D5 37
+#define D6 38
+#define D7 39
 
 void calculateRPM();
 
-int avgRPM;
+LiquidCrystal lcd(LCD_RST, LCD_EN, D4, D5, D6, D7);
+
+volatile double avgRPM;
+volatile unsigned int count = 0;
+volatile unsigned int lastCount = 0;
+volatile unsigned long lastMillis; 
 
 void setup() 
 {
@@ -28,21 +38,33 @@ void setup()
 
   digitalWrite(LED, HIGH);
   digitalWrite(IR_VCC, HIGH);
+  lcd.begin(16, 2);
 }
 
 void loop() 
 {
   // put your main code here, to run repeatedly:
   digitalWrite(DRIVER_EN, HIGH);
-  attachInterrupt(digitalPinToInterrupt(RPM_INT), calculateRPM, HIGH);
+  lastMillis = millis();
+  attachInterrupt(digitalPinToInterrupt(RPM_INT), calculateRPM, CHANGE);
+
+  if(avgRPM < (0.75 * 200))
+  {
+    digitalWrite(FAIL_LED, HIGH);
+  }
+
+  lcd.print(avgRPM);
+  delay(500);
 }
 
 void calculateRPM()
 {
-  int arr[IR_FREQ] = {};
-  int count = 0;
+  
+  int arr[40] = {};
 
-  for(int i = 0; i < IR_FREQ; i++)
+  lastCount = count;
+
+  for(int i = 0; i < 40; i++)
   {
 
     arr[i] = digitalRead(RPM_INT);
@@ -53,7 +75,10 @@ void calculateRPM()
     }
   }
 
-  int avgRPM = count / IR_FREQ;
-  Serial.println(avgRPM);
+  unsigned long currentMillis = millis();
+  unsigned long dt = currentMillis - lastMillis;
 
+
+  int dCount = count - lastCount;
+  avgRPM = 60 * (double) dCount / (double) dt; // this math makes no sense
 }
